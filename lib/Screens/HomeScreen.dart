7af85f9583fa301929199810dca7792eec19ac1f
DevangSharma <debusharma1999@gmail.com>
2021-01-10 10:33:03 +0530

    UI skeleton done

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -4,27 +4,100 @@ import 'drawer.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        appBar: AppBar(
-    title: Text("Helping Hand Foundation"),),
-    
-        drawer: DrawerList(),
+      appBar: AppBar(
+        title: Text("Helping Hand Foundation"),
+      ),
+      drawer: DrawerList(),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: screenHeight*0.02),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ListTile(
+                title: Text("Last Payment done"),
+                subtitle: Text("Date : 2 Jan. 2021"),
+                leading: Icon(Icons.timer),
+              ),
+              
+              
 
-         body: Column(  
-        children: [  
-         ListTile(
-           title: Text("Last Payment done"),
-           subtitle: Text("Date : 2 Jan. 2021"),
-           leading: Icon(Icons.timer),
-         ),
+              ListTile(
+                title: Text("Next Payment"),
+                subtitle: Text("Date : 2 Feb. 2021"),
+                leading: Icon(Icons.timer),
+              ),
+              
+              Container(
+                height: screenHeight*0.02,
+              ),
 
-         Text("Monthly Plans"),
-         Text("Enroll in a monthly plan"),
-         Text("Donate Us"),
-         Text("Donate"),
+              Padding(
+                padding: EdgeInsets.fromLTRB(screenWidth * 0.1,
+                    screenHeight * 0.01, screenWidth * 0.1, screenHeight * 0.01),
+                child: Table(
+                  children: [
+                    TableRow(children: [
+                      TableCell(child: Center(child: Text("Plan Name"))),
+                      TableCell(child: Center(child: Text("Monthly Charges"))),
+                      TableCell(child: Center(child: Text("Daily Worth"))),
+                    ]),
+                    TableRow(children: [
+                      TableCell(child: Center(child: Text("Hello"))),
+                      TableCell(child: Center(child: Text("Hello"))),
+                      TableCell(child: Center(child: Text("Hello"))),
+                    ]),
+                    TableRow(children: [
+                      TableCell(child: Center(child: Text("Hello"))),
+                      TableCell(child: Center(child: Text("Hello"))),
+                      TableCell(child: Center(child: Text("Hello"))),
+                    ]),
+                    TableRow(children: [
+                      TableCell(child: Center(child: Text("Hello"))),
+                      TableCell(child: Center(child: Text("Hello"))),
+                      TableCell(child: Center(child: Text("Hello"))),
+                    ]),
+                  ],
+                ),
+              ),
 
-        ],  
-      ),  
-      );
+              Container(
+                height: screenHeight*0.02,
+              ),
+
+              RaisedButton(
+                color: Colors.amber,
+                onPressed: () {},
+                child: Text("Register for monthly plan"),
+                ),
+
+                Container(
+                height: screenHeight*0.02,
+              ),
+
+          
+        
+              RaisedButton(
+                color: Colors.amber,
+                onPressed: () {},
+                child: Text("One Time Donation"),
+                ),
+
+                Container(
+                height: screenHeight*0.02,
+              ),
+
+                    Text("!! सेवा ही जीवन है !!", style: TextStyle(
+                      fontSize: 30,
+                    ),),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/Screens/drawer.dart
+++ lib/Screens/drawer.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DrawerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return Drawer(
-        child: ListView(
+      // column holds all the widgets in the drawer
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            // ListView contains a group of widgets that scroll inside the drawer
+            child: ListView(
           padding: const EdgeInsets.all(0.0),
           children: [
             UserAccountsDrawerHeader(
-              accountName: Text("Ashish Rawat"),
-              accountEmail: Text("ashishrawat2911@gmail.com"),
+              accountName: Text("Devang Sharma"),
+              accountEmail: Text("debusharma1999@gmail.com"),
               currentAccountPicture: CircleAvatar(
                 backgroundColor:
                     Theme.of(context).platform == TargetPlatform.iOS
                         ? Colors.blue
                         : Colors.white,
                 child: Text(
-                  "A",
+                  "DS",
                   style: TextStyle(fontSize: 40.0),
                 ),
               ),
@@ -24,12 +31,17 @@ class DrawerList extends StatelessWidget {
 
             ListTile(
               leading: Icon(Icons.person),
-              title: Text("About Us"),
+              title: Text("Our Mission"),
             ),
 
             ListTile(
               leading: Icon(Icons.language),
               title: Text("Our Website"),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.image),
+              title: Text("Gallery"),
             ),
 
             ListTile(
@@ -42,18 +54,24 @@ class DrawerList extends StatelessWidget {
               title: Text("Connect Us on Social Media"),
             ),
 
-            Container(),
 
-            Container(
-              child: Row(
-                children: [
-                  IconButton(icon: Icon(Icons.phone), onPressed: null),
-                  IconButton(icon: Icon(Icons.mail), onPressed: null),
-                ],
-              ),
-            )
+            
           ],
         ),
-      );
+          ),
+          // This container holds the align
+          Container(
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(icon: Icon(Icons.phone), onPressed: ()  {}),
+                  IconButton(icon: Icon(Icons.mail), onPressed: (){}),
+                ],
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
